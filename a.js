344e87dddf960284c3b11520a9083cd062a76b69
zhangyicheng <1239271010@qq.com>
2018-03-07 22:25:06 +0800

    add html js css

---
+++ a.js
@@ -0,0 +1,22 @@
+var calc = document.getElementById('rectangle__calc');
+calc.onclick = function(){
+  var width = document.getElementById('rectangle__width');
+  var height = document.getElementById('rectangle__height');
+  var per = document.getElementById('rectangle__perimeter');
+  var are = document.getElementById('rectangle__area');
+  if(width.value !== "" && height.value !== ""){
+    per.value = width.value * 2 + height.value * 2;
+    are.value = width.value * height.value;
+  }else{
+    document.getElementById('tx1').innerHTML = "*";
+    document.getElementById('tx2').innerHTML = "*";
+  }
+  width.onfocus = function(){
+    document.getElementById('tx1').innerHTML = "";
+    document.getElementById('tx2').innerHTML = "";
+  }
+  height.onfocus = function(){
+    document.getElementById('tx1').innerHTML = "";
+    document.getElementById('tx2').innerHTML = "";
+  }
+}
