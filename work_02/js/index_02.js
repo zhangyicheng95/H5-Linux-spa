5d3df18ef6a43291ac31870fb710facb71fa6cd0
zhangyicheng <1239271010@qq.com>
2018-03-08 20:08:34 +0800

    updata

--- work_02/js/index_02.js
+++ work_02/js/index_02.js
@@ -1,1 +1,10 @@
-f
+var uneye = $('#uneye');
+var inp = $('#inp');
+uneye.mouseover(function(){
+  uneye[0].src = "img/eye100.png";
+  inp.attr('type','textbox');
+});
+uneye.mouseout(function(){
+  uneye[0].src = "img/eye101.png";
+  inp.attr('type','password');
+})
