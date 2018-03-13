36efcc5f828421e287cb426ca2ae6f038399dd6c
zhangyicheng95 <1239271010@qq.com>
2018-03-13 17:19:45 +0800

    updata

--- work_01/index.js
+++ work_01/index.js
@@ -1,42 +1,49 @@
 window.onload = function(){
-  document.getElementById('rectangle__width').focus();
-
-var calc = document.getElementById('rectangle__calc');
-calc.onclick = function(){
-  var width = document.getElementById('rectangle__width');
-  var height = document.getElementById('rectangle__height');
-  var per = document.getElementById('rectangle__perimeter');
-  var are = document.getElementById('rectangle__area');
-  /*字符级校验*/
-  width.oninput = function(){
-    if(typeof width.value !== Number || width.value<=0 || height.value<=0){      width.value = "";
-      document.getElementById('tx1').innerHTML = "请输入正数值"; 
-    }
-  }
-  /*字段级校验*/
-  width.onblur = function(){
-    if(width.value !== ""){
-      height.focus;
-    }
-  }
-  /*表单级校验*/
-  if(width.value !== "" && height.value !== "" && width.value > 0 && height.value > 0){
-    var per_result = JSON.stringify((Number(width.value) * 2 + Number(height.value) * 2));
-    per.value = parseFloat(per_result.slice(0,per_result.indexOf('.')+3));
-    var are_result = JSON.stringify(Number(width.value) * Number(height.value));
-    are.value = parseFloat(are_result.slice(0,are_result.indexOf('.')+3));
-  
-  }else{
-    document.getElementById('tx1').innerHTML = "*";
-    document.getElementById('tx2').innerHTML = "*";
-  }
-  width.onfocus = function(){
-    document.getElementById('tx1').innerHTML = "";
-    document.getElementById('tx2').innerHTML = "";
-  }
-  height.onfocus = function(){
-    document.getElementById('tx1').innerHTML = "";
-    document.getElementById('tx2').innerHTML = "";
-  }
-}
+  document.getElementById('rectangle__width').focus();//页面加载完成后自动获取焦点
+	var calc = document.getElementById('rectangle__calc');
+	calc.onclick = function(){
+	  var width = document.getElementById('rectangle__width');
+	  var height = document.getElementById('rectangle__height');
+	  var per = document.getElementById('rectangle__perimeter');
+	  var are = document.getElementById('rectangle__area');
+	  /*字符级校验*/
+	  width.oninput = function(){
+	    if(typeof width.value == String){      
+	    	width.value = "";
+	      document.getElementById('tx1').innerHTML = "请输入正数值"; 
+	    }
+	  }
+	  /*字段级校验*/
+	  width.onblur = function(){
+	    if(width.value == ""){
+	      width.onfocus;
+	    }
+	  }
+	  /*表单级校验*/
+	  if(width.value !== "" && height.value !== "" && width.value > 0 && height.value > 0){
+	    var per_result = JSON.stringify((Number(width.value) * 2 + Number(height.value) * 2));
+	    if(per_result.indexOf('.')>0){
+	    	per.value = parseFloat(per_result.slice(0,per_result.indexOf('.')+3));
+	    }else{
+	    	per.value = parseFloat(per_result);
+	    }
+	    var are_result = JSON.stringify(Number(width.value) * Number(height.value));
+	    if(are_result.indexOf('.')>0){
+	    	are.value = parseFloat(are_result.slice(0,are_result.indexOf('.')+3));
+	  	}else{
+	  		are.value = parseFloat(are_result);
+	  	}
+	  }else{
+	    document.getElementById('tx1').innerHTML = "*";
+	    document.getElementById('tx2').innerHTML = "*";
+	  }
+	  width.onfocus = function(){
+	    document.getElementById('tx1').innerHTML = "";
+	    document.getElementById('tx2').innerHTML = "";
+	  }
+	  height.onfocus = function(){
+	    document.getElementById('tx1').innerHTML = "";
+	    document.getElementById('tx2').innerHTML = "";
+	  }
+	}
 }
