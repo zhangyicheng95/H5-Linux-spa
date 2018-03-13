2d9af7af52f0e14e8829edfed9b64ea4bb089e95
zhangyicheng <1239271010@qq.com>
2018-03-13 16:02:20 +0800

    updata

--- work_02/js/index_04.js
+++ work_02/js/index_04.js
@@ -2,11 +2,13 @@ var start = document.getElementById('start');
 var susp = document.getElementById('suspended');
 var stop = document.getElementById('stop');
 var val = document.getElementById('prg');
+var timer;
 start.onclick = function(){
-  var timer = setInterval(function(){
+  timer !== null && clearInterval(timer);
+  timer = setInterval(function(){
     val.value++;
     document.getElementById('result').innerHTML = val.value + '%';
-  },1000);
+  },300);
   susp.onclick = function(){
     clearInterval(timer);
   };
