bc0430725f966a33074a30e9b8561be8ad1cb355
zhangyicheng95 <1239271010@qq.com>
2018-03-15 14:49:45 +0800

    updata

--- work_01/index.js
+++ work_01/index.js
@@ -1,12 +1,10 @@
 window.onload = function(){
   document.getElementById('rectangle__width').focus();//页面加载完成后自动获取焦点
 	var calc = document.getElementById('rectangle__calc');
-	calc.onclick = function(){
-	  var width = document.getElementById('rectangle__width');
-	  var height = document.getElementById('rectangle__height');
-	  var per = document.getElementById('rectangle__perimeter');
-	  var are = document.getElementById('rectangle__area');
-	  /*字符级校验*/
+	var width = document.getElementById('rectangle__width');
+	 var height = document.getElementById('rectangle__height');
+
+	/*字符级校验*/
 	  width.oninput = function(){
 	    if(typeof width.value == String){      
 	    	width.value = "";
@@ -14,11 +12,18 @@ window.onload = function(){
 	    }
 	  }
 	  /*字段级校验*/
-	  width.onblur = function(){
+	 $('rectangle__width').blur(function(){
 	    if(width.value == ""){
-	      width.onfocus;
+	      width.focus();
 	    }
-	  }
+	  })
+
+	calc.onclick = function(){
+	  var width = document.getElementById('rectangle__width');
+	  var height = document.getElementById('rectangle__height');
+	  var per = document.getElementById('rectangle__perimeter');
+	  var are = document.getElementById('rectangle__area');
+
 	  /*表单级校验*/
 	  if(width.value !== "" && height.value !== "" && width.value > 0 && height.value > 0){
 	    var per_result = JSON.stringify((Number(width.value) * 2 + Number(height.value) * 2));
