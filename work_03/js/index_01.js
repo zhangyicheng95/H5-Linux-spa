b925a0406725a7ecd885209175c0c9c97ad529d9
zhangyicheng95 <1239271010@qq.com>
2018-03-15 16:06:13 +0800

    update

--- work_03/js/index_01.js
+++ work_03/js/index_01.js
@@ -2,8 +2,9 @@ $('#btn').click(function(){
   if($('#txt').val() !== ""){
     $('#ul').append("<li>"+$('#txt').val()+"<img src='img/write.png' id='img'/>"+"</li>");
     $('#txt').val("");
+    $('#img').click(function(){
+      $('#txt').focus();
+    })
   }
 });
-$('#img').click(function(){
-  $('#txt').focus();
-})
+
