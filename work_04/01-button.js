768d724a046a10cfb82226a7e861d3b5538b0762
zhangyicheng95 <1239271010@qq.com>
2018-03-20 17:05:14 +0800

    update

---
+++ work_04/01-button.js
@@ -0,0 +1,13 @@
+
+  //实例化定时器按钮
+
+  var $btn=timerButton({
+  'container':'div.main',
+  'title':'同意',
+  'tLength':9,
+  'enable':false
+  });
+  $btn.on('timer-button-click',function(){
+    alert('同意了！');
+  });
+  
---
+++ work_04/01-index.js
@@ -0,0 +1,32 @@
+var timerButton = function(config){
+  var $btn = $('<input type="button" id="btn-timer-button" >'),
+  timer,
+  t=config.tLength;
+  $(config.container).append($btn);
+  
+  $('#btn').click(function(){
+      $btn.trigger('timer-button-click');
+  });
+
+  if(config.enable){
+    enableBtn();
+  }else{
+    disableBtn();
+  }
+
+  function enableBtn(){
+    $btn.val(config.title);
+    $btn.removeAttr('disabled');
+  }
+  function disableBtn(){
+    $btn.attr('disabled',"disabled");
+    timer = window.setInterval(function(){
+      $btn.val(config.title + '(' + t-- + 's)');
+      if(t<0){
+        enableBtn();
+        window.clearInterval(timer);
+      }
+    },1000)
+  }
+  return $btn;
+}
