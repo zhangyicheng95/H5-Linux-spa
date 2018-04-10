c9630d5e6537dad776e77721479497c6760df2db
zhangyicheng95 <1239271010@qq.com>
2018-04-10 16:42:26 +0800

    update

--- work_05/app/js/p-change-password.js
+++ work_05/app/js/p-change-password.js
@@ -1,5 +1,5 @@
 var $changePwdPanel = (function(){
-	function show(){
+	function show(config){
 		$(app.config.panelCotainer).html('<h1>$changePwdPanel</h1>')
 	};
 	return {show:show};
--- work_05/app/js/p-store-list.js
+++ work_05/app/js/p-store-list.js
@@ -1,5 +1,5 @@
 var $storeListPanel = (function(){
-	function show(){
+	function show(config){
 		$(app.config.panelCotainer).html('<h1>$storeListPanel</h1>')
 	};
 	return {show:show};
