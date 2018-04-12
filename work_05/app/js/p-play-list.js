e6223f3268839204b2edba7156bcee524d5d92a2
zhangyicheng95 <1239271010@qq.com>
2018-04-12 16:22:03 +0800

    update

--- work_05/app/js/p-play-list.js
+++ work_05/app/js/p-play-list.js
@@ -1,7 +1,7 @@
 var $playListPanel = (function() {
   function show(config) {
-    $(app.config.panelContainer).html('<h1>$playListPanel</h1>');
+    $(app.config.panelContainer).html('');
+    $(app.config.panelContainer).append('<h1>$playListPanel</h1>');
   }
-
   return {show: show};
 })();
