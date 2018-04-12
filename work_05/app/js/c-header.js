c533c74045276ed76d6a2ae992f031905d1b7bb1
zhangyicheng95 <1239271010@qq.com>
2018-04-12 16:05:19 +0800

    update

--- work_05/app/js/c-header.js
+++ work_05/app/js/c-header.js
@@ -5,41 +5,46 @@ var $header = (function(){
 	+'	<a class="iconfont icon-daohang"></a>'
 	+'</div>'
 	+'<div class="account">'
-	+'	<div class="account-menu">'
-		+'	<i class="logo"></i>'
-		+'	<span>管理</span>'
-		+'	<i class="iconfont icon-arrowdown" id="i1"></i>'
-	+'	</div>'
+	+'	<i class="logo"></i>'
+	+'	<span>管理</span>'
+	+'	<i class="iconfont icon-arrowdown" id="i1"></i>'
 	+'	<div class="submenu">'
 	+'		<i class="iconfont icon-icon_logout"></i>'
 	+'		<span>注销</span>'
 	+'	</div>'
 	+'</div>';
 	
+	var $title = $('.title h1'),
+		$logo = $('.account .logo'),
+		$account = $('.account'),
+		$menu = $('.submenu'),
+		$arrow = $('#i1');
+	
+	function onMouseOver() {
+		$menu.css('display','block');
+		$arrow.addClass('icon-arrowup');
+		$arrow.removeClass('icon-arrowdown');
+	}
+	
+	function onMouseOut() {
+		$menu.css('display','none');
+		$arrow.addClass('icon-arrowdown');
+		$arrow.removeClass('icon-arrowup');
+	}
+	
+	function onClick() {
+		onMouseOut();
+		location.hash = '#/logout';
+	}
+	
 	function show(config){
 		$(app.config.appContainer).append('<div class="admin-app-header">'+$headerDOM+'</div>');
-		
-		var $title = $('.title h1'),
-			$logo = $('.account .logo'),
-			$account = $('.account-menu'),
-			$menu = $('.submenu'),
-			$arrow = $('#i1');
 		$title.html(app.config.headerTitle);
 		$logo.css('background','url('+app.config.headerLogo+')');
+		$account.mouseover(onMouseOver);
+	    $account.mouseout(onMouseOut);
+	    $menu.click(onClick);
 		
-		$account.mouseover(function(){
-			$menu.css('display','block');
-			$arrow.addClass('icon-arrowup');
-			$arrow.removeClass('icon-arrowdown');
-		});
-		$menu.mouseout(function(){
-			$menu.css('display','none');
-			$arrow.addClass('icon-arrowdown');
-			$arrow.removeClass('icon-arrowup');
-		});
-		$menu.click(function(){
-			location.hash = '#/login';
-		});
 	}
 	return {show:show};
 })();
