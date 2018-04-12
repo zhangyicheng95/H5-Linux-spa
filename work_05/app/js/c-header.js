0b113db66ee1ab1e2f20a1982ba5ffe4e352df55
zhangyicheng95 <1239271010@qq.com>
2018-04-12 16:15:02 +0800

    3

--- work_05/app/js/c-header.js
+++ work_05/app/js/c-header.js
@@ -5,46 +5,42 @@ var $header = (function(){
 	+'	<a class="iconfont icon-daohang"></a>'
 	+'</div>'
 	+'<div class="account">'
-	+'	<i class="logo"></i>'
-	+'	<span>管理</span>'
-	+'	<i class="iconfont icon-arrowdown" id="i1"></i>'
+	+'	<div class="account_menu">'	
+	+'		<div class="logo"></div>'
+	+'		<span>管理</span>'
+	+'		<i class="iconfont icon-arrowdown" id="i1"></i>'
+	+'	</div>'
 	+'	<div class="submenu">'
 	+'		<i class="iconfont icon-icon_logout"></i>'
 	+'		<span>注销</span>'
 	+'	</div>'
 	+'</div>';
 	
-	var $title = $('.title h1'),
-		$logo = $('.account .logo'),
-		$account = $('.account'),
-		$menu = $('.submenu'),
-		$arrow = $('#i1');
-	
-	function onMouseOver() {
-		$menu.css('display','block');
-		$arrow.addClass('icon-arrowup');
-		$arrow.removeClass('icon-arrowdown');
-	}
-	
-	function onMouseOut() {
-		$menu.css('display','none');
-		$arrow.addClass('icon-arrowdown');
-		$arrow.removeClass('icon-arrowup');
-	}
-	
-	function onClick() {
-		onMouseOut();
-		location.hash = '#/logout';
-	}
-	
 	function show(config){
+		$(app.config.appContainer).append('<div class="admin-app-header">'+$headerDOM+'</div>');
+		
+		var $title = $('.title h1'),
+			$logo = $('.account .logo'),
+			$account = $('.account'),
+			$account_menu = $('.account_menu'),
+			$menu = $('.submenu'),
+			$arrow = $('#i1');
 		$title.html(app.config.headerTitle);
 		$logo.css('background','url('+app.config.headerLogo+')');
-		$(app.config.appContainer).append('<div class="admin-app-header">'+$headerDOM+'</div>');
-		$account.mouseover(onMouseOver);
-	    $account.mouseout(onMouseOut);
-	    $menu.click(onClick);
 		
+		$account_menu.mouseover(function(){
+			$menu.css('display','block');
+			$arrow.addClass('icon-arrowup');
+			$arrow.removeClass('icon-arrowdown');
+		});
+		$account.mouseout(function(){
+			$menu.css('display','none');
+			$arrow.addClass('icon-arrowdown');
+			$arrow.removeClass('icon-arrowup');
+		});
+		$menu.click(function(){
+			location.hash = '#/login';
+		});
 	}
 	return {show:show};
 })();
